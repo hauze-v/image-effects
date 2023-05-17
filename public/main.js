17004c1566768b7b56c3d79570f2b0ef0c4e91c1
hauze-v <hauze_v@protonmail.com>
2023-05-17 17:57:24 -0600

    Finished project. Rdy for deploy

--- public/main.js
+++ public/main.js
@@ -1,4 +1,13 @@
-function init() {
+async function init() {
+  let rustApp = null;
+
+  try {
+    rustApp = await import('../pkg');
+  } catch(err) {
+    console.error(err)
+    return;
+  }
+
   const input = document.getElementById('upload');
   // Allows us to save file uploaded by the user in string format instead of binary (input.files[0])
   const fileReader = new FileReader();
@@ -8,6 +17,11 @@ function init() {
     const base64 = fileReader.result.replace( // tag on this replace method to remove the prepended metadata before the encoding
       /^data:image\/(png|jpeg|jpg);base64,/, ''
     );
+
+    let img_data_url = rustApp.grayscale(base64);
+    document.getElementById('new-img').setAttribute(
+      'src', img_data_url
+    )
   }
 
   input.addEventListener('change', () => {
